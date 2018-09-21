9e5a0f2b5d1bb41268bd0088673a08769f0f09f4
Sanjay Govind <sanjay.govind9@gmail.com>
2018-09-22 01:40:14 +1200

    Memes

--- android/src/main/java/com/reactlibrary/RNMemesModule.java
+++ android/src/main/java/com/reactlibrary/RNMemesModule.java
@@ -7,6 +7,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 import java.util.Arrays;
@@ -14,28 +15,31 @@ import java.util.Arrays;
 public class RNMemesModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
-  private final Visualizer v;
+  private Visualizer v;
 
   public RNMemesModule(final ReactApplicationContext reactContext) {
     super(reactContext);
+
     this.reactContext = reactContext;
-    this.v = new Visualizer(0);
-    this.v.setCaptureSize(512);
-    this.v.setDataCaptureListener(new Visualizer.OnDataCaptureListener() {
-        @Override
-        public void onWaveFormDataCapture(Visualizer visualizer, byte[] waveform, int samplingRate) {
-            String data = Base64.encodeToString(waveform,0);
-            reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                    .emit("audio", data);
-        }
+      }
+  @ReactMethod
+  public void beginRecording() {
+      this.v = new Visualizer(0);
+      this.v.setCaptureSize(512);
+      this.v.setDataCaptureListener(new Visualizer.OnDataCaptureListener() {
+          @Override
+          public void onWaveFormDataCapture(Visualizer visualizer, byte[] waveform, int samplingRate) {
+              String data = Base64.encodeToString(waveform,0);
+              reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                      .emit("audio", data);
+          }
 
-        @Override
-        public void onFftDataCapture(Visualizer visualizer, byte[] fft, int samplingRate) {
+          @Override
+          public void onFftDataCapture(Visualizer visualizer, byte[] fft, int samplingRate) {
 
-        }
-    },16000, true, false);
+          }
+      },16000, true, false);
   }
-
   @Override
   public String getName() {
     return "RNMemes";
